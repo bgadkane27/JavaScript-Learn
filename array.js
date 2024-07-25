539967ca785230ea0f928f5610c0d52532140757
Baburao Adkane <b.g.adkane27@gmail.com>
2024-07-25 19:00:30 +0530

    new files

--- array.js
+++ array.js
@@ -3,3 +3,17 @@ const removed = fruits.splice(2, 2, 'fig', 'grape');
 console.log(`The original array after splice method operations: ${fruits}`); 
 
 console.log(`The array after splice method operations: ${removed}`); 
+
+
+let myArr=[1,5,7,8,9,0,9,25,22];
+console.log("A", myArr[2]);
+
+// myArr.push(30);
+// myArr.pop();
+// console.log("B", myArr);
+
+// myArr.unshift(30);
+// console.log("C", myArr);
+
+// myArr.shift();
+// console.log("D",myArr);
